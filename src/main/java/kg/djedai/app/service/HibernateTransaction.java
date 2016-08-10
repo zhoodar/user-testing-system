c622b862819525f8f910604fcfa8d76bff98e379
ZHoodar <ajoodar@gmail.com>
2016-08-10 23:36:05 +0600

    added LocalSessionFactoryBean of spring to handle session of the hibernate, the old HibernateTransaction  is modified to a suggested new  one

--- src/main/java/kg/djedai/app/service/HibernateTransaction.java
+++ src/main/java/kg/djedai/app/service/HibernateTransaction.java
@@ -2,38 +2,20 @@ package kg.djedai.app.service;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Zhoodar
  * @since 05.08.2016.
  */
-public class HibernateTransaction {
+@Service
+public abstract class HibernateTransaction {
 
-    private final SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory  sessionFactory;
 
-    public HibernateTransaction() {
-        sessionFactory = new Configuration().configure().buildSessionFactory();
+    protected Session getCurrentSession(){
+        return sessionFactory.getCurrentSession();
     }
-
-    public interface Command<E>{
-        E process(Session session);
-    }
-
-    public <E> E transaction(final Command<E> command){
-        final Session session = sessionFactory.openSession();
-        final Transaction tn = session.beginTransaction();
-        try{
-            return command.process(session);
-        } finally {
-            tn.commit();
-            session.close();
-        }
-    }
-
-    public void close(){
-        this.sessionFactory.close();
-    }
-
 }
